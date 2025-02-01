1f9c8443e5337e15a30b2dd90063ee7efd2111fe
Romanumo <romanumo1@gmail.com>
2025-01-31 23:20:34 -0800

    Small Fixes

--- Assets/Scripts/Editor/EditorFramerate.cs
+++ Assets/Scripts/Editor/EditorFramerate.cs
@@ -6,11 +6,11 @@ using UnityEditor;
 [InitializeOnLoad]
 public class EditorFramerate : Editor
 {
-	static EditorFramerate() => EditorApplication.update += Update;
+	/*static EditorFramerate() => EditorApplication.update += Update;
 
 	static void Update()
 	{
 		if (Application.isPlaying)
 			Application.targetFrameRate = 60;
-	}
+	}*/
 }
